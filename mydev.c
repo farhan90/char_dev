abf3604ffcd2ddea28de89b8de24288a7f05fd9c
Farhan <farhan@Farhans-MacBook-Pro.local>
2015-10-18 21:39:09 -0400

    Added device read functionality

--- mydev.c
+++ mydev.c
@@ -4,11 +4,18 @@
 #include <linux/device.h> /*class_create, device_create*/
 #include <linux/types.h> /*dev_t*/
 #include <linux/fs.h> /* struct file_operatiosn*/
-
+#include <linux/string.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/kernel.h> /* snprintf*/
+#include <linux/time.h> /*current_kernel_time */
 
 static dev_t dev; /*To get the device number*/
 static struct cdev c_dev; /* The char device */
 static struct class *cl; /* Device class*/
+
+static char* data_buff;
+static int data_buff_len=100;
 
 int dev_open(struct inode *i, struct file *file){
 	printk("FARHAN: Opening my char device\n");
@@ -17,10 +24,33 @@ int dev_open(struct inode *i, struct file *file){
 
 ssize_t dev_read(struct file *f, char __user *ubuf, 
 			size_t len,loff_t *off){
-
+	
+	struct timespec ts;
+	int ret=0;
 	printk("FARHAN: Reading my char device\n");
-	return 0;
 
+	ts=current_kernel_time();
+	
+	ret=snprintf(data_buff,data_buff_len,
+				"Current Kernel Time: %lu \n",ts.tv_sec);
+	if(copy_to_user(ubuf,data_buff,data_buff_len)<0){
+		printk("FARHAN: Failed to copy to user\n");
+		kfree(data_buff);
+		return -1;
+	}
+		
+	return ret;
+
+}
+
+static int init_data(void){
+	
+	data_buff=kmalloc(data_buff_len,GFP_KERNEL);
+	if (!data_buff){
+		printk("FARHAN: No memory\n");
+		return -ENOMEM;
+	}
+	return 0;
 }
 
 /* Defining file operations of my device */
@@ -59,6 +89,10 @@ static int mydev_init(void){
 		goto rm_cdev;
 	}
 
+	if(init_data()<0){
+		printk("FARHAN: Error init data\n");
+		goto rm_cdev;
+	}
 	printk("FARHAN : Finished registering the device\n");
 	return 0;	
 
@@ -82,6 +116,9 @@ static void mydev_exit(void){
 	device_destroy(cl,dev);
 	class_destroy(cl);
 	unregister_chrdev_region(dev,1);
+	if(data_buff){
+		kfree(data_buff);
+	}	
 	return;
 }
 
